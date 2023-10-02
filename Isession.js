da47581548384648bf25873f18f103e6716548b3
GABRIEL HENRIQUE MOREIRA ALVES <60203348+LordBland@users.noreply.github.com>
2023-10-02 17:59:05 -0300

    begin session

---
+++ Isession.js
@@ -0,0 +1,43 @@
+function pm(eventObject) {
+    (function () {
+      function APIMM(url, parametros) {
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(parametros)
+        })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (resultado) {
+          console.log(resultado);
+        })
+        .catch(function (erro) {
+          console.error(erro);
+        });
+      }
+  
+      function geraIDUnico() {
+        // Código para geração do ID único
+        // ...
+      }
+  
+      var sessionId = 'SE-' + geraIDUnico();
+      var hash = geraIDUnico();
+    
+      sessionStorage.setItem('MMPsessionId', sessionId);
+      sessionStorage.setItem('MMhash', hash);
+      
+      var Tpointer = 'P-' + hash + '-' + Date.now()
+  
+      var urlDaAPI = "https://pixel-monks-hg62gkdjeq-rj.a.run.app/hit";
+      var parametros = {
+        method: 'POST',
+        body: JSON.stringify(eventObject)
+      };
+  
+      APIMM(urlDaAPI, eventObject);
+    })();
+  }
