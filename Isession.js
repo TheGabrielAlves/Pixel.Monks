7af64feedc8c60c22dd3fad1a4bb91309780e922
GABRIEL HENRIQUE MOREIRA ALVES <60203348+LordBland@users.noreply.github.com>
2023-10-03 09:06:30 -0300

    Update Isession.js

--- Isession.js
+++ Isession.js
@@ -1,12 +1,6 @@
-function pm(eventObject) {
-   console.log('Início da função pm'); // Adiciona log no início
-   function APIMM(url, eventObjectLocal) {
-    console.log('Início da função APIMM'); // Adiciona log no início
-    // Restante do código...
-    console.log('Fim da função APIMM'); // Adiciona log no final
-  }
+console.log('Script carregado!');
 
-  // Restante do código...
-  
-  console.log('Fim da função pm'); // Adiciona log no final
+function pm(eventObject) {
+    console.log('pm chamada');
+    // Restante do código...
 }
