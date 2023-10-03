e713057cd2797c5acd1657d5b928be50f2699aa9
GABRIEL HENRIQUE MOREIRA ALVES <60203348+LordBland@users.noreply.github.com>
2023-10-03 09:00:37 -0300

    Update Isession.js

--- Isession.js
+++ Isession.js
@@ -1,61 +1,12 @@
 function pm(eventObject) {
-  function APIMM(url, eventObjectLocal) {
-  consolo.log("Deu certo 1")
-   fetch(url, {
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     body: JSON.stringify(eventObjectLocal)
-   })
-     .then(function (response) {
-       return response.json();
-     })
-     .then(function (resultado) {
-       console.log(resultado);
-     })
-     .catch(function (erro) {
-       console.error(erro);
-     });
- }
+   console.log('Início da função pm'); // Adiciona log no início
+   function APIMM(url, eventObjectLocal) {
+    console.log('Início da função APIMM'); // Adiciona log no início
+    // Restante do código...
+    console.log('Fim da função APIMM'); // Adiciona log no final
+  }
 
- //Gera o session id.................................................
- function geraIDUnico() {
-   var letters = 'abcdefghijklmnopqrstuvwxyz';
-   var randomLetter = letters.charAt(Math.floor(Math.random() * letters.length));
-   var timestamp = Date.now().toString(36);
-   var parteAleatoria = Math.random().toString(36).substr(2, 5);
-   var idUnico = randomLetter + '-' + timestamp + parteAleatoria
-   return idUnico;
-}
-
-
-
-  try {
-    var sessionId = 'SE-' + geraIDUnico();
-
-    var hash = geraIDUnico();
-   
-    sessionStorage.setItem('MMPsessionId', sessionId);
-    sessionStorage.setItem('MMhash', hash);
-    
-    var Tpointer = 'P-' + hash + '-' + Date.now()
-  eventObjectLocal = {
-   eventName: eventObject.eventName,
-   client: eventObject.client,  // definir um codigo unico para o cliente
-   sessionId: sessionId,
-   deviceId: undefined,
-   url: eventObject.url,
-   ref: document.referrer,
-   pointers: Tpointer,
-   params:eventObject.params,
- };}
- catch(e){console.log("DEU ERRO: "+ e)}
- 
- //..................................................................
- 
- var urlDaAPI = "https://pixel-monks-hg62gkdjeq-rj.a.run.app/hit";
-
- APIMM(urlDaAPI, eventObjectLocal);
-
+  // Restante do código...
+  
+  console.log('Fim da função pm'); // Adiciona log no final
 }
